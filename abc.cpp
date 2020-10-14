603570663b1809c1c85bfd6cf5300d32e9dc81bc
Dikshant <dikshantniraula351@gmail.com>
2020-10-14 20:30:43 +0545

    initial commit

---
+++ abc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    /* My first program in C */
+    cin >> number;
+    cout << "you entered, " << number << "\n";
+    return 0;
+}
