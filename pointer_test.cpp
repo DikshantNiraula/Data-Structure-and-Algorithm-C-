2839695e459e143bbfbc032e737df95ebbb5ce97
Dikshant <dikshantniraula351@gmail.com>
2020-10-20 06:14:05 +0545

    Pointer Test

---
+++ pointer_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1 = 1, var2 = 4;
+
+    cout << "Address of var1 is " << &var1 << endl;
+    cout << "Address of var2 is " << &var2 << endl;
+}
