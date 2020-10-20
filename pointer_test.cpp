b2e3dc398c3a9b0cfe58043c28ea00a199a9a8f2
Dikshant <dikshantniraula351@gmail.com>
2020-10-20 06:17:22 +0545

    Pointer Test with output

--- pointer_test.cpp
+++ pointer_test.cpp
@@ -8,3 +8,7 @@ int main()
     cout << "Address of var1 is " << &var1 << endl;
     cout << "Address of var2 is " << &var2 << endl;
 }
+
+//output
+// Address of var1 is 0x7ffe11347170
+// Address of var2 is 0x7ffe11347174
